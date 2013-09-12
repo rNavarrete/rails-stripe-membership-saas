c1596847f10cfd3ac40adb44980d346bc44b92b2
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-09-12 13:22:56 -0700

    added stripe webhooks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,91 @@
-class UsersController < ApplicationController
-  before_filter :authenticate_user!
+class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-  def index
-    authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all
-  end
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :stripe_token, :coupon
+  attr_accessor :stripe_token, :coupon
+  before_save :update_stripe
+  before_destroy :cancel_subscription
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def update
-    authorize! :update, @user, :message => 'Not authorized as an administrator.'
-    @user = User.find(params[:id])
-    role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
-    params[:user] = params[:user].except(:role_ids)
-    if @user.update_attributes(params[:user])
-      @user.update_plan(role) unless role.nil?
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
+  def update_plan(role)
+    self.role_ids = []
+    self.add_role(role.name)
+    unless customer_id.nil?
+      customer = Stripe::Customer.retrieve(customer_id)
+      customer.update_subscription(:plan => role.name)
     end
+    true
+  rescue Stripe::StripeError => e
+    logger.error "Stripe Error: " + e.message
+    errors.add :base, "Unable to update your subscription. #{e.message}."
+    false
   end
 
-  def destroy
-    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
-      redirect_to users_path, :notice => "User deleted."
+  def update_stripe
+    return if email.include?(ENV['ADMIN_EMAIL'])
+    return if email.include?('@example.com') and not Rails.env.production?
+    if customer_id.nil?
+      if !stripe_token.present?
+        raise "Stripe token not present. Can't create account."
+      end
+      if coupon.blank?
+        customer = Stripe::Customer.create(
+          :email => email,
+          :description => name,
+          :card => stripe_token,
+          :plan => roles.first.name
+        )
+      else
+        customer = Stripe::Customer.create(
+          :email => email,
+          :description => name,
+          :card => stripe_token,
+          :plan => roles.first.name,
+          :coupon => coupon
+        )
+      end
     else
-      redirect_to users_path, :notice => "Can't delete yourself."
+      customer = Stripe::Customer.retrieve(customer_id)
+      if stripe_token.present?
+        customer.card = stripe_token
+      end
+      customer.email = email
+      customer.description = name
+      customer.save
     end
+    self.last_4_digits = customer.active_card.last4
+    self.customer_id = customer.id
+    self.stripe_token = nil
+  rescue Stripe::StripeError => e
+    logger.error "Stripe Error: " + e.message
+    errors.add :base, "#{e.message}."
+    self.stripe_token = nil
+    false
   end
+
+  def cancel_subscription
+    unless customer_id.nil?
+      customer = Stripe::Customer.retrieve(customer_id)
+      unless customer.nil? or customer.respond_to?('deleted')
+        if customer.subscription.status == 'active'
+          customer.cancel_subscription
+        end
+      end
+    end
+  rescue Stripe::StripeError => e
+    logger.error "Stripe Error: " + e.message
+    errors.add :base, "Unable to cancel your subscription. #{e.message}."
+    false
+  end
+
+  def expire
+    UserMailer.expire_email(self).deliver
+    destroy
+  end
+
 end
