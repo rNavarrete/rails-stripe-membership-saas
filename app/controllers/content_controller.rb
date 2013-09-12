a9561f5c295ddc7321b4fd46ecf1fa66dc4566bc
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-09-11 18:25:42 -0700

    added registration

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,10 +1,14 @@
 class ContentController < ApplicationController
+	
   def silver
+    authorize! :view, :silver, :message => 'Access limited to Silver Plan subscribers.'
   end
 
   def gold
+    authorize! :view, :gold, :message => 'Access limited to Gold Plan subscribers.'
   end
 
   def platinum
+    authorize! :view, :platinum, :message => 'Access limited to Platinum Plan subscribers.'
   end
 end
